4ded6264da69cab65ce9c0f5c8b40bcb1cb024f9
David <davidbrandon430@gmail.com>
2019-11-20 12:02:17 +0000

    Works

--- get-quote.py
+++ get-quote.py
@@ -6,9 +6,12 @@ def main():
   quotes = f.readlines()
   f.close()
 
-  last = 13
+  last = 16
   rnd = random.randint(0, last)
   print(quotes[rnd])
+  print("\n".join(quotes)) #https://stackoverflow.com/questions/6167731/printing-list-elements-on-separated-lines-in-python
+
+
 
 if __name__== "__main__":
   main()
